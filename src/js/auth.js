b71a6d9454ee64e55122ea433ddf0a13fe8d527d
Ulla Sederlöf <ulla.sederlof@metropolia.fi>
2025-02-24 22:41:56 +0200

    add auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,2 +1,58 @@
 import '../css/style.css';
 import '../css/snackbar.css';
+import {fetchData} from './fetch.js';
+
+console.log('Moi luodaan nyt tokeneita ja kirjaudutaan sisään');
+
+// Esimerkin takia haut ovat nyt suoraan tässä tiedostossa, jotta harjoitus ei sekoita
+// teidän omaa projektin rakennetta
+
+const registerUser = async (event) => {
+  event.preventDefault();
+
+  // Haetaan oikea formi
+  const registerForm = document.querySelector('.registerForm');
+
+  // Haetaan formista arvot
+  const username = registerForm.querySelector('#username').value.trim();
+  const password = registerForm.querySelector('#password').value.trim();
+  const email = registerForm.querySelector('#email').value.trim();
+
+  // Luodaan body lähetystä varten taustapalvelun vaatimaan muotoon
+  const bodyData = {
+    username: username,
+    password: password,
+    email: email,
+  };
+
+  // Endpoint
+  const url = 'http://localhost:3000/api/users';
+
+  // Options
+  const options = {
+    body: JSON.stringify(bodyData),
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+  console.log(options);
+
+  // Hae data
+  const response = await fetchData(url, options);
+
+  if (response.error) {
+    console.error('Error adding a new user:', response.error);
+    return;
+  }
+
+  if (response.message) {
+    console.log(response.message, 'success');
+  }
+
+  console.log(response);
+  registerForm.reset(); // tyhjennetään formi
+};
+
+const registerForm = document.querySelector('.registerForm');
+registerForm.addEventListener('submit', registerUser);
